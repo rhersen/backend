401cad7bd0f6c9d09d8cc20b7b3a2da75ac5d311
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-10-17 17:52:25 +0200

    Refactor tests

--- test/getHtml-test.js
+++ test/getHtml-test.js
@@ -7,58 +7,60 @@ describe('getHtml', function () {
         expect(getHtml()).to.match(/<title>Pendeltåg<.title>/)
     })
 
+    const announcements = [{
+        'ActivityType': 'Ankomst',
+        'AdvertisedTimeAtLocation': '2016-06-28T21:52:00',
+        'AdvertisedTrainIdent': '2769',
+        'LocationSignature': 'Åbe',
+        'ToLocation': [{'LocationName': 'Söc', 'Priority': 1, 'Order': 0}],
+        'TimeAtLocation': '2016-06-28T21:57:00'
+    }, {
+        'ActivityType': 'Ankomst',
+        'AdvertisedTimeAtLocation': '2016-06-28T22:19:00',
+        'AdvertisedTrainIdent': '2870',
+        'LocationSignature': 'Åbe',
+        'ToLocation': [{'LocationName': 'Spå', 'Priority': 1, 'Order': 0}],
+        'TimeAtLocation': '2016-06-28T22:20:00'
+    }, {
+        'ActivityType': 'Ankomst',
+        'AdvertisedTimeAtLocation': '2016-06-28T22:10:00',
+        'AdvertisedTrainIdent': '2868',
+        'LocationSignature': 'Spå',
+        'ToLocation': [{'LocationName': 'Spå', 'Priority': 1, 'Order': 0}],
+        'TimeAtLocation': '2016-06-28T22:08:00'
+    }, {
+        'ActivityType': 'Ankomst',
+        'AdvertisedTimeAtLocation': '2016-06-28T22:10:00',
+        'AdvertisedTrainIdent': '2866',
+        'LocationSignature': 'Spå',
+        'ToLocation': [{'LocationName': 'Spå', 'Priority': 1, 'Order': 0}],
+        'TimeAtLocation': '2016-06-28T22:09:00'
+    }, {
+        'ActivityType': 'Avgang',
+        'AdvertisedTimeAtLocation': '2016-06-28T22:06:00',
+        'AdvertisedTrainIdent': '2864',
+        'LocationSignature': 'Sub',
+        'ToLocation': [{'LocationName': 'Spå', 'Priority': 1, 'Order': 0}],
+        'TimeAtLocation': '2016-06-28T22:06:00'
+    }]
+
     it('arrival three minutes late', function () {
-        expect(getHtml([{
-            'ActivityType': 'Ankomst',
-            'AdvertisedTimeAtLocation': '2016-06-28T21:52:00',
-            'AdvertisedTrainIdent': '2769',
-            'LocationSignature': 'Åbe',
-            'ToLocation': [{'LocationName': 'Söc', 'Priority': 1, 'Order': 0}],
-            'TimeAtLocation': '2016-06-28T21:55:00'
-        }])).to.match(/Tåg 2769 mot Söc ankom till Åbe 3 minuter försenat klockan 21:55/)
+        expect(getHtml(announcements)).to.match(/Tåg 2769 mot Söc ankom till Åbe 5 minuter försenat klockan 21:57/)
     })
 
     it('departure one minute late', function () {
-        expect(getHtml([{
-            'ActivityType': 'Ankomst',
-            'AdvertisedTimeAtLocation': '2016-06-28T22:19:00',
-            'AdvertisedTrainIdent': '2870',
-            'LocationSignature': 'Åbe',
-            'ToLocation': [{'LocationName': 'Spå', 'Priority': 1, 'Order': 0}],
-            'TimeAtLocation': '2016-06-28T22:20:00'
-        }])).to.match(/Tåg 2870 mot Spå ankom till Åbe nästan i tid klockan 22:20/)
+        expect(getHtml(announcements)).to.match(/Tåg 2870 mot Spå ankom till Åbe nästan i tid klockan 22:20/)
     })
 
     it('early arrival', function () {
-        expect(getHtml([{
-            'ActivityType': 'Ankomst',
-            'AdvertisedTimeAtLocation': '2016-06-28T22:10:00',
-            'AdvertisedTrainIdent': '2868',
-            'LocationSignature': 'Spå',
-            'ToLocation': [{'LocationName': 'Spå', 'Priority': 1, 'Order': 0}],
-            'TimeAtLocation': '2016-06-28T22:08:00'
-        }])).to.match(/Tåg 2868 mot Spå ankom till Spå i god tid klockan 22:08/)
+        expect(getHtml(announcements)).to.match(/Tåg 2868 mot Spå ankom till Spå i god tid klockan 22:08/)
     })
 
     it('one minute is not really early arrival', function () {
-        expect(getHtml([{
-            'ActivityType': 'Ankomst',
-            'AdvertisedTimeAtLocation': '2016-06-28T22:10:00',
-            'AdvertisedTrainIdent': '2868',
-            'LocationSignature': 'Spå',
-            'ToLocation': [{'LocationName': 'Spå', 'Priority': 1, 'Order': 0}],
-            'TimeAtLocation': '2016-06-28T22:09:00'
-        }])).to.match(/Tåg 2868 mot Spå ankom till Spå i tid klockan 22:09/)
+        expect(getHtml(announcements)).to.match(/Tåg 2866 mot Spå ankom till Spå i tid klockan 22:09/)
     })
 
     it('departure on time', function () {
-        expect(getHtml([{
-            'ActivityType': 'Avgang',
-            'AdvertisedTimeAtLocation': '2016-06-28T22:06:00',
-            'AdvertisedTrainIdent': '2868',
-            'LocationSignature': 'Sub',
-            'ToLocation': [{'LocationName': 'Spå', 'Priority': 1, 'Order': 0}],
-            'TimeAtLocation': '2016-06-28T22:06:00'
-        }])).to.match(/Tåg 2868 mot Spå avgick från Sub i tid klockan 22:06/)
+        expect(getHtml(announcements)).to.match(/Tåg 2864 mot Spå avgick från Sub i tid klockan 22:06/)
     })
 })
