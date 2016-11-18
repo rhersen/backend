bed0a39d58494b62cf6c64bb4a078948e8dfc54f
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-11-18 07:49:51 +0100

    Move announcementQuery to server.js

--- announcements.js
+++ announcements.js
@@ -1,7 +1,5 @@
 const http = require('http')
 
-const key = require('./key')
-
 function sendRequest(postData) {
     return outgoingResponse => {
         const options = {
@@ -44,40 +42,6 @@ function sendRequest(postData) {
     }
 }
 
-function announcementQuery(filters) {
-    return `<REQUEST>
-     <LOGIN authenticationkey='${key}' />
-     <QUERY objecttype='TrainAnnouncement' lastmodified='true' orderBy='AdvertisedTimeAtLocation'>
-      <FILTER>
-       <AND>
-        <IN name='ProductInformation' value='Pendeltåg' />
-        <NE name='Canceled' value='true' />
-        ${filters}
-       </AND>
-      </FILTER>
-      <INCLUDE>LocationSignature</INCLUDE>
-      <INCLUDE>AdvertisedTrainIdent</INCLUDE>
-      <INCLUDE>AdvertisedTimeAtLocation</INCLUDE>
-      <INCLUDE>EstimatedTimeAtLocation</INCLUDE>
-      <INCLUDE>TimeAtLocation</INCLUDE>
-      <INCLUDE>ToLocation</INCLUDE>
-      <INCLUDE>ActivityType</INCLUDE>
-     </QUERY>
-    </REQUEST>`
-}
-
 module.exports = {
-    current: sendRequest(announcementQuery(`
-        <GT name='TimeAtLocation' value='$dateadd(-0:12:00)' />
-        <LT name='TimeAtLocation' value='$dateadd(0:12:00)' />`)),
-
-    ingela: sendRequest(announcementQuery(`
-        <OR>
-          <EQ name='LocationSignature' value='Tul' />
-          <EQ name='LocationSignature' value='Åbe' />
-          <EQ name='LocationSignature' value='Sub' />
-        </OR>
-        <GT name='AdvertisedTimeAtLocation' value='$dateadd(-0:28:00)' />
-        <LT name='AdvertisedTimeAtLocation' value='$dateadd(0:28:00)' />`
-    ))
+    sendRequest: sendRequest
 }
--- server.js
+++ server.js
@@ -1,15 +1,48 @@
 const atob = require('atob')
 
-const stations = require('./stations')
 const announcements = require('./announcements')
+const key = require('./key')
+const stations = require('./stations')
+
+function announcementQuery(filters) {
+    return `<REQUEST>
+     <LOGIN authenticationkey='${key}' />
+     <QUERY objecttype='TrainAnnouncement' lastmodified='true' orderBy='AdvertisedTimeAtLocation'>
+      <FILTER>
+       <AND>
+        <IN name='ProductInformation' value='Pendeltåg' />
+        <NE name='Canceled' value='true' />
+        ${filters}
+       </AND>
+      </FILTER>
+      <INCLUDE>LocationSignature</INCLUDE>
+      <INCLUDE>AdvertisedTrainIdent</INCLUDE>
+      <INCLUDE>AdvertisedTimeAtLocation</INCLUDE>
+      <INCLUDE>EstimatedTimeAtLocation</INCLUDE>
+      <INCLUDE>TimeAtLocation</INCLUDE>
+      <INCLUDE>ToLocation</INCLUDE>
+      <INCLUDE>ActivityType</INCLUDE>
+     </QUERY>
+    </REQUEST>`
+}
 
 function requestListener(incomingRequest, outgoingResponse) {
     const url = decodeURIComponent(incomingRequest.url)
 
     if (/current/.test(url))
-        announcements.current(outgoingResponse)
+        announcements.sendRequest(announcementQuery(`
+        <GT name='TimeAtLocation' value='$dateadd(-0:12:00)' />
+        <LT name='TimeAtLocation' value='$dateadd(0:12:00)' />`))(outgoingResponse)
     else if (/ingela/.test(url))
-        announcements.ingela(outgoingResponse)
+        announcements.sendRequest(announcementQuery(`
+        <OR>
+          <EQ name='LocationSignature' value='Tul' />
+          <EQ name='LocationSignature' value='Åbe' />
+          <EQ name='LocationSignature' value='Sub' />
+        </OR>
+        <GT name='AdvertisedTimeAtLocation' value='$dateadd(-0:28:00)' />
+        <LT name='AdvertisedTimeAtLocation' value='$dateadd(0:28:00)' />`
+        ))(outgoingResponse)
     else if (/stations/.test(url))
         stations.json(outgoingResponse)
     else
