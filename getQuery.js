ec03447cef7d3822fd98ecbed980159c6ba4a153
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-12-05 08:28:14 +0100

    Refactor

--- getQuery.js
+++ getQuery.js
@@ -1,14 +1,14 @@
 const query = require('./query')
 
 module.exports = url => {
-    let q
     let match
 
     if (/current/.test(url))
-        q = query.current()
-    else if (/ingela/.test(url))
-        q = query.trains('1:30:00', ['Tul', 'Åbe', 'Sub'])
-    else if (match = /train.(\d\d\d\d)/.exec(url))
-        q = query.train(match[1])
-    return q
+        return query.current()
+
+    if (/ingela/.test(url))
+        return query.trains('1:30:00', ['Tul', 'Åbe', 'Sub'])
+
+    if (match = /train.(\d\d\d\d)/.exec(url))
+        return query.train(match[1])
 }
