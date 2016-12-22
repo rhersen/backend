e33ee79c58bf56a7c00161f9a36037e49ea80494
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-12-22 17:31:58 +0100

    Use array instead of multiline string for filters

--- query.js
+++ query.js
@@ -8,7 +8,7 @@ function announcementQuery(filters) {
        <AND>
         <IN name='ProductInformation' value='Pendeltåg' />
         <NE name='Canceled' value='true' />
-        ${filters}
+        ${filters.join('\n')}
        </AND>
       </FILTER>
       <INCLUDE>LocationSignature</INCLUDE>
@@ -24,40 +24,38 @@ function announcementQuery(filters) {
 
 
 function like(direction) {
-    if (!direction)
-        return ''
-
     return `<LIKE name='AdvertisedTrainIdent' value='/[${direction === 'n' ? '02468' : '13579'}]$/' />`
 }
 
 
 module.exports = {
-    current: () => announcementQuery(`
-        <GT name='TimeAtLocation' value='$dateadd(-0:12:00)' />
-        <LT name='TimeAtLocation' value='$dateadd(0:12:00)' />`),
+    current: () => announcementQuery([
+        "<GT name='TimeAtLocation' value='$dateadd(-0:12:00)' />",
+        "<LT name='TimeAtLocation' value='$dateadd(0:12:00)' />"]),
 
-    trains: (locations, since, until, direction) => announcementQuery(`
-        ${(like(direction))}
-        <OR> ${locations.map(location => `<EQ name='LocationSignature' value='${location}' />`).join(' ')} </OR>
-        <OR>
-         <AND>
-          <GT name='AdvertisedTimeAtLocation' value='$dateadd(-${since}:00)' />
-          <LT name='AdvertisedTimeAtLocation' value='$dateadd(${until}:00)' />
-         </AND>
-         <AND>
-          <GT name='EstimatedTimeAtLocation' value='$dateadd(-${since}:00)' />
-          <LT name='EstimatedTimeAtLocation' value='$dateadd(${until}:00)' />
-         </AND>
-         <AND>
-          <GT name='TimeAtLocation' value='$dateadd(-${since}:00)' />
-          <LT name='TimeAtLocation' value='$dateadd(${until}:00)' />
-         </AND>
-        </OR>`
-    ),
+    trains: (locations, since, until, direction) => {
+        const filters = [
+            `<OR> ${locations.map(location => `<EQ name='LocationSignature' value='${location}' />`).join(' ')} </OR>`,
+            `<OR>
+             <AND>
+              <GT name='AdvertisedTimeAtLocation' value='$dateadd(-${since}:00)' />
+              <LT name='AdvertisedTimeAtLocation' value='$dateadd(${until}:00)' />
+             </AND>
+             <AND>
+              <GT name='EstimatedTimeAtLocation' value='$dateadd(-${since}:00)' />
+              <LT name='EstimatedTimeAtLocation' value='$dateadd(${until}:00)' />
+             </AND>
+             <AND>
+              <GT name='TimeAtLocation' value='$dateadd(-${since}:00)' />
+              <LT name='TimeAtLocation' value='$dateadd(${until}:00)' />
+             </AND>
+            </OR>`]
 
-    train: id => announcementQuery(`
-        <EQ name='AdvertisedTrainIdent' value='${id}' />
-        <GT name='TimeAtLocation' value='$dateadd(-0:12:00)' />
-        <LT name='TimeAtLocation' value='$dateadd(0:12:00)' />`
-    )
+        return announcementQuery(direction ? [like(direction).concat(filters)] : filters)
+    },
+
+    train: id => announcementQuery([
+        `<EQ name='AdvertisedTrainIdent' value='${id}' />`,
+        "<GT name='TimeAtLocation' value='$dateadd(-0:12:00)' />",
+        "<LT name='TimeAtLocation' value='$dateadd(0:12:00)' />"])
 }
