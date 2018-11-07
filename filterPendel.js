b5c2cc3f0d338df94e4d70ae189e5f235578fdec
Rudolf Hersén <rudolf.hersen@schibsted.se>
2018-11-07 22:39:33 +0100

    Add SL siteId to "pendel" items (if there's a local LineData.json file)

--- filterPendel.js
+++ filterPendel.js
@@ -2,12 +2,18 @@ const filter = require('lodash/fp/filter');
 const includes = require('lodash/fp/includes');
 const flow = require('lodash/fp/flow');
 const map = require('lodash/fp/map');
+const max = require('lodash/fp/max');
+const reduce = require('lodash/fp/reduce');
+const replace = require('lodash/fp/replace');
 const uniq = require('lodash/fp/uniq');
 
+const lineData = require('./LineData');
+
 module.exports = (trains, stations) => {
-  const locations = flow(map('LocationSignature'), uniq)(
-    get(trains, 'TrainAnnouncement')
-  );
+  const locations = flow(
+    map('LocationSignature'),
+    uniq
+  )(get(trains, 'TrainAnnouncement'));
 
   return flow(
     filter(station => includes(station.LocationSignature, locations)),
@@ -15,10 +21,39 @@ module.exports = (trains, stations) => {
   )(get(stations, 'TrainStation'));
 
   function eastNorth(station) {
+    const sites = reduce(
+      (found, name) => {
+        if (found) {
+          return found;
+        }
+        const matches = filter(
+          site => site.SiteName === name(station),
+          lineData.ResponseData.Result
+        );
+        if (matches.length) {
+          return matches;
+        }
+      },
+      undefined,
+      [
+        stn => stn.AdvertisedShortLocationName,
+        stn => replace(/ C$/, ' central', stn.AdvertisedLocationName),
+        stn => replace(/ C$/, ' centralstation', stn.AdvertisedLocationName),
+      ]
+    );
+
+    const siteId = flow(
+      map(site => {
+        const id = site.SiteId;
+        return id.length > 4 ? id.substr(id.length - 4) : id;
+      }),
+      max
+    )(sites);
+
     const match = /POINT \(([\d\\.]+) ([\d\\.]+)\)/.exec(
       station.Geometry.WGS84
     );
-    return { ...station, east: match[1], north: match[2] };
+    return { ...station, east: match[1], north: match[2], siteId };
   }
 };
 
