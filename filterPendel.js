1d8e9b0d78134f909c8937f8b1c4d8c1c93ef07f
Rudolf Hersén <rudolf.hersen@schibsted.se>
2018-11-08 07:32:35 +0100

    Refactor to handle missing LineData

--- filterPendel.js
+++ filterPendel.js
@@ -1,6 +1,7 @@
 const filter = require('lodash/fp/filter');
 const includes = require('lodash/fp/includes');
 const flow = require('lodash/fp/flow');
+const get = require('lodash/fp/get');
 const map = require('lodash/fp/map');
 const max = require('lodash/fp/max');
 const reduce = require('lodash/fp/reduce');
@@ -13,50 +14,62 @@ module.exports = (trains, stations) => {
   const locations = flow(
     map('LocationSignature'),
     uniq
-  )(get(trains, 'TrainAnnouncement'));
+  )(result(trains, 'TrainAnnouncement'));
 
   return flow(
     filter(station => includes(station.LocationSignature, locations)),
     map(eastNorth)
-  )(get(stations, 'TrainStation'));
+  )(result(stations, 'TrainStation'));
 
   function eastNorth(station) {
-    const sites = reduce(
-      (found, name) => {
-        if (found) {
-          return found;
-        }
-        const matches = filter(
-          site => site.SiteName === name(station),
-          lineData.ResponseData.Result
-        );
-        if (matches.length) {
-          return matches;
-        }
-      },
-      undefined,
-      [
-        stn => stn.AdvertisedShortLocationName,
-        stn => replace(/ C$/, ' central', stn.AdvertisedLocationName),
-        stn => replace(/ C$/, ' centralstation', stn.AdvertisedLocationName),
-      ]
-    );
-
-    const siteId = flow(
-      map(site => {
-        const id = site.SiteId;
-        return id.length > 4 ? id.substr(id.length - 4) : id;
-      }),
-      max
-    )(sites);
-
     const match = /POINT \(([\d\\.]+) ([\d\\.]+)\)/.exec(
       station.Geometry.WGS84
     );
-    return { ...station, east: match[1], north: match[2], siteId };
+
+    return {
+      ...station,
+      east: match[1],
+      north: match[2],
+      siteId: siteId(get('ResponseData.Result', lineData)),
+    };
+
+    function siteId(allSites) {
+      if (!allSites) {
+        return;
+      }
+
+      const sites = reduce(
+        (found, name) => {
+          if (found) {
+            return found;
+          }
+          const matches = filter(
+            site => site.SiteName === name(station),
+            allSites
+          );
+          if (matches.length) {
+            return matches;
+          }
+        },
+        undefined,
+        [
+          stn => stn.AdvertisedShortLocationName,
+          stn => replace(/ C$/, ' central', stn.AdvertisedLocationName),
+          stn => replace(/ C$/, ' centralstation', stn.AdvertisedLocationName),
+        ]
+      );
+
+      return flow(
+        map(site => {
+          const id = site.SiteId;
+          return id.length > 4 ? id.substr(id.length - 4) : id;
+        }),
+        max
+      )(sites);
+    }
   }
 };
 
-function get(trafikinfo, field) {
+function result(trafikinfo, field) {
   return trafikinfo.RESPONSE.RESULT[0][field];
 }
