cdcf3d5bf2b73d4759a924dd78805a26a5f0be1b
Rudolf Hersén <rudolf.hersen@schibsted.se>
2018-12-18 16:48:26 +0100

    Extract each request into its own function

--- pendel.js
+++ pendel.js
@@ -9,61 +9,105 @@ const cache = {
   lineData: false,
 };
 
+async function getStations() {
+  try {
+    if (!cache.stations) {
+      const stations = await request
+        .post('http://api.trafikinfo.trafikverket.se/v1.2/data.json')
+        .type('xml')
+        .send(stationQuery());
+      cache.stations = JSON.parse(stations.text);
+    }
+  } catch (e) {
+    console.log(e);
+    cache.stations = false;
+  }
+
+  return cache.stations;
+
+  function stationQuery() {
+    return `<REQUEST>
+     <LOGIN authenticationkey='${key.trafikverket}' />
+     <QUERY objecttype='TrainStation'>
+      <FILTER>
+       <OR>
+         <IN name='CountyNo' value='1' />
+         <IN name='CountyNo' value='2' />
+         <IN name='CountyNo' value='3' />
+         <IN name='CountyNo' value='4' />
+       </OR>
+      </FILTER>
+      <INCLUDE>LocationSignature</INCLUDE>
+      <INCLUDE>AdvertisedLocationName</INCLUDE>
+      <INCLUDE>AdvertisedShortLocationName</INCLUDE>
+      <INCLUDE>Geometry</INCLUDE>
+     </QUERY>
+    </REQUEST>`;
+  }
+}
+
+async function getTrains() {
+  try {
+    if (!cache.trains) {
+      const trains = await request
+        .post('http://api.trafikinfo.trafikverket.se/v1.2/data.json')
+        .type('xml')
+        .send(trainQuery());
+      cache.trains = JSON.parse(trains.text);
+    }
+  } catch (e) {
+    console.log(e);
+    cache.trains = false;
+  }
+
+  return cache.trains;
+
+  function trainQuery() {
+    return `<REQUEST>
+     <LOGIN authenticationkey='${key.trafikverket}' />
+     <QUERY objecttype='TrainAnnouncement' lastmodified='true' orderby='AdvertisedTimeAtLocation'>
+      <FILTER>
+       <AND>
+        <IN name='ProductInformation' value='Pendeltåg' />
+        <EQ name='ActivityType' value='Avgang' />
+        <GT name='AdvertisedTimeAtLocation' value='$dateadd(0:00:00)' />
+        <LT name='AdvertisedTimeAtLocation' value='$dateadd(0:59:00)' />
+       </AND>
+      </FILTER>
+      <INCLUDE>LocationSignature</INCLUDE>
+     </QUERY>
+    </REQUEST>`;
+  }
+}
+
+async function getLineData() {
+  try {
+    if (!cache.lineData) {
+      const lineData = await request.get(
+        `https://api.sl.se/api2/LineData.json?model=site&key=${
+          key.slSites
+        }&DefaultTransportModeCode=TRAIN`
+      );
+      cache.lineData = JSON.parse(lineData.text);
+    }
+  } catch (e) {
+    console.log(e);
+    cache.lineData = false;
+  }
+
+  return cache.lineData;
+}
+
 module.exports = {
   json: async function(outgoingResponse) {
-    const start = new Date();
-    console.log(start);
-
-    try {
-      if (!cache.stations) {
-        const stations = await request
-          .post('http://api.trafikinfo.trafikverket.se/v1.2/data.json')
-          .type('xml')
-          .send(stationQuery());
-        cache.stations = JSON.parse(stations.text);
-      }
-    } catch (e) {
-      console.log(e);
-      cache.stations = false;
-    }
-
-    console.log('station response after', new Date() - start, 'ms');
-
-    try {
-      if (!cache.trains) {
-        const trains = await request
-          .post('http://api.trafikinfo.trafikverket.se/v1.2/data.json')
-          .type('xml')
-          .send(trainQuery());
-        cache.trains = JSON.parse(trains.text);
-      }
-    } catch (e) {
-      console.log(e);
-      cache.trains = false;
-    }
-
-    console.log('trains response after', new Date() - start, 'ms');
-
-    try {
-      if (!cache.lineData) {
-        const lineData = await request.get(
-          `https://api.sl.se/api2/LineData.json?model=site&key=${
-            key.slSites
-          }&DefaultTransportModeCode=TRAIN`
-        );
-        cache.lineData = JSON.parse(lineData.text);
-      }
-    } catch (e) {
-      console.log(e);
-      cache.lineData = false;
-    }
-
-    console.log('line data response after', new Date() - start, 'ms');
-
     try {
       respond(
         JSON.stringify(
-          filterPendel(cache.trains, cache.stations, cache.lineData)
+          filterPendel(
+            await getTrains(),
+            await getStations(),
+            await getLineData()
+          )
         ),
         outgoingResponse
       );
@@ -84,40 +128,3 @@ module.exports = {
     }
   },
 };
-
-function stationQuery() {
-  return `<REQUEST>
-     <LOGIN authenticationkey='${key.trafikverket}' />
-     <QUERY objecttype='TrainStation'>
-      <FILTER>
-       <OR>
-         <IN name='CountyNo' value='1' />
-         <IN name='CountyNo' value='2' />
-         <IN name='CountyNo' value='3' />
-         <IN name='CountyNo' value='4' />
-       </OR>
-      </FILTER>
-      <INCLUDE>LocationSignature</INCLUDE>
-      <INCLUDE>AdvertisedLocationName</INCLUDE>
-      <INCLUDE>AdvertisedShortLocationName</INCLUDE>
-      <INCLUDE>Geometry</INCLUDE>
-     </QUERY>
-    </REQUEST>`;
-}
-
-function trainQuery() {
-  return `<REQUEST>
-     <LOGIN authenticationkey='${key.trafikverket}' />
-     <QUERY objecttype='TrainAnnouncement' lastmodified='true' orderby='AdvertisedTimeAtLocation'>
-      <FILTER>
-       <AND>
-        <IN name='ProductInformation' value='Pendeltåg' />
-        <EQ name='ActivityType' value='Avgang' />
-        <GT name='AdvertisedTimeAtLocation' value='$dateadd(0:00:00)' />
-        <LT name='AdvertisedTimeAtLocation' value='$dateadd(0:59:00)' />
-       </AND>
-      </FILTER>
-      <INCLUDE>LocationSignature</INCLUDE>
-     </QUERY>
-    </REQUEST>`;
-}
